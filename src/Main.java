b6429321acc9f169bdb213b6badf9886b4dd5b85
Deepak Kannan <kannan.deepak@gmail.com>
2015-11-18 16:04:48 +0530

    cannot get inner-bean

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,15 @@ public class Main {
 
     public static void main(String[] args) {
         ApplicationContext context = new FileSystemXmlApplicationContext("spring.xml");
+
+        Point point = null;
+        try {
+            point = (Point) context.getBean("inner-bean");
+        } catch (Exception e) {
+            System.out.println("cannot refer to inner bean");
+        }
+        System.out.println("inner bean: " + point);
+
         Triangle shape = (Triangle) context.getBean("triangle");
 
         shape.draw();
