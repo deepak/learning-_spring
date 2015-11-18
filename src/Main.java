7cd95c56123a92aaf47de8447fb314216f426159
Deepak Kannan <kannan.deepak@gmail.com>
2015-11-18 16:23:24 +0530

    going overboard with bean name and alias

--- src/Main.java
+++ src/Main.java
@@ -14,8 +14,27 @@ public class Main {
         }
         System.out.println("inner bean: " + point);
 
-        Triangle shape = (Triangle) context.getBean("triangle");
+        Triangle shape1 = (Triangle) context.getBean("triangle");
+        Triangle shape2 = (Triangle) context.getBean("shape");
+        Triangle shape3 = (Triangle) context.getBean("t1");
+        Triangle shape4 = (Triangle) context.getBean("s1");
+        Triangle shape5 = (Triangle) context.getBean("t2");
 
-        shape.draw();
+        System.out.println("shape1: ");
+        shape1.draw();
+
+        System.out.println("shape2: ");
+        shape2.draw();
+
+        System.out.println("shape3: ");
+        shape3.draw();
+
+        System.out.println("shape4: ");
+        shape4.draw();
+
+        System.out.println("shape5: ");
+        System.out.println(shape5);
+
+        //System.out.println("both beans are equal: " + shape1.equals(shape2));
     }
 }
