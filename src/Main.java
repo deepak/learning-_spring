6f25bca8b55317e1f9a7995338b19fc1e0b8662f
Deepak Kannan <kannan.deepak@gmail.com>
2015-11-19 13:53:58 +0530

    singleton bean scope applies only to a container
    singleton bean scope is not for the whole JVm
    but applies only to a container
    so if we create another container, with the same config files
    we will get different objects from the singleton bean

--- src/Main.java
+++ src/Main.java
@@ -5,16 +5,21 @@ public class Main {
 
     public static void main(String[] args) {
         ApplicationContext context = new FileSystemXmlApplicationContext("spring.xml");
+        ApplicationContext newContext = new FileSystemXmlApplicationContext("spring.xml");
 
         Triangle shape1 = (Triangle) context.getBean("triangle");
         Triangle shape2 = (Triangle) context.getBean("triangle");
+
+        Triangle newShape = (Triangle) newContext.getBean("triangle");
 
         Point point1 = (Point) context.getBean("point-0");
         Point point2 = (Point) context.getBean("point-0");
 
         shape1.draw();
 
-        System.out.println("shapes are equal: " + shape1.equals(shape2));
+        System.out.println("shapes in same container are equal: " + shape1.equals(shape2));
+        System.out.println("shapes in diff. container are equal: " + newShape.equals(shape1));
+
         System.out.println("point in shapes are equal: " + shape1.getPointA().equals(shape2.getPointA()));
         System.out.println("points are equal: " + point1.equals(point2));
 
