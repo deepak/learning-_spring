141beee8101fd88c1daf930be721b5f3abe74b3a
Deepak Kannan <kannan.deepak@gmail.com>
2015-11-19 13:14:17 +0530

    explore equality of a prototype scope bean

--- src/Main.java
+++ src/Main.java
@@ -7,6 +7,15 @@ public class Main {
         ApplicationContext context = new FileSystemXmlApplicationContext("spring.xml");
 
         Triangle shape1 = (Triangle) context.getBean("triangle");
+        Triangle shape2 = (Triangle) context.getBean("triangle");
+
+        Point point1 = (Point) context.getBean("point-0");
+        Point point2 = (Point) context.getBean("point-0");
+
         shape1.draw();
+
+        System.out.println("shapes are equal: " + shape1.equals(shape2));
+        System.out.println("point in shapes are equal: " + shape1.getPointA().equals(shape2.getPointA()));
+        System.out.println("points are equal: " + point1.equals(point2));
     }
 }
