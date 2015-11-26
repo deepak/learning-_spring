cc2324a24940c2c654d6acefb90517443ec647c1
Deepak Kannan <kannan.deepak@gmail.com>
2015-11-26 15:53:59 +0530

    cannot get @Value annotation to work even though @Autowired works

--- src/org/deepak/springdemo/db/DbAppConfig.java
+++ src/org/deepak/springdemo/db/DbAppConfig.java
@@ -1,12 +1,15 @@
 package org.deepak.springdemo.db;
 
 import org.apache.commons.dbcp2.BasicDataSource;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.event.ContextClosedEvent;
 import org.springframework.context.event.EventListener;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 
+import javax.sql.DataSource;
 import java.sql.SQLException;
 
 @Configuration
@@ -24,6 +27,16 @@ public class DbAppConfig {
         dataSource.setPassword(password);
 
         return dataSource;
+    }
+
+    // TODO: @Value("dataSource") does not work here
+    // even though it works in OfferDAO
+    // @Autowired does work though
+    @Bean
+    @Autowired
+    public DataSourceTransactionManager dataSourceTransactionManager(DataSource dataSource) {
+        System.out.println("datasource: " + dataSource);
+        return new DataSourceTransactionManager(dataSource);
     }
 
     @EventListener
